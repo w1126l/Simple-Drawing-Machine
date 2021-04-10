25c61327abe53d319831729592051924611d3cca
Winnie Liang <1126winniel@gmail.com>
2021-04-09 19:30:28 -0700

    for 3.0 for now

--- sketch.js
+++ sketch.js
@@ -2,32 +2,45 @@
 var canvas;
 var bgR = 80; bgG = 100; bgB = 200;
 
+let lineR, lineG, lineB;
+let lineWeight;
+
 let input;
 let img;
 let imgAdd;
 let imgSave;
 let imgReset;
 
-let lineColor = 255;
-let bgColor = 125;
-let lineWeight = 5;
-let lineTrig = 0;
-
 function setup() {
 
   frameRate(60);
-  canvas = createCanvas((windowWidth - 400), (windowHeight - 200));
+  canvas = createCanvas((windowWidth - 600), (windowHeight - 300));
   canvas.parent('sketch-holder');
 
-  background(bgR, bgG, bgB);
-  stroke(lineColor);
-  strokeWeight(lineWeight);
+  bgR = createSlider(0, 255, 180);
+  bgR.position(10, 20);
+  bgG = createSlider(0, 255, 100);
+  bgG.position(10, 40);
+  bgB = createSlider(0, 255, 200);
+  bgB.position(10, 60);
 
-  imgSave = createButton('Save Image');
+  background(bgR.value(), bgG.value(), bgB.value());
+
+  lineR = createSlider(0, 255, 200);
+  lineR.position(10, 120);
+  lineG = createSlider(0, 255, 200);
+  lineG.position(10, 140);
+  lineB = createSlider(0, 255, 180);
+  lineB.position(10, 160);
+
+  lineWeight = createSlider(1, 50, 5);
+  lineWeight.position(10, 220);
+
+  imgSave = createButton('Save Image As a png');
   imgSave.parent('save-button');
   imgSave.mousePressed(saveImage);
 
-  imgReset = createButton('Reset Image');
+  imgReset = createButton('Reset Image/Update New Background Color');
   imgReset.parent('reset-button');
   imgReset.mousePressed(reset);
 
@@ -35,15 +48,40 @@ function setup() {
 
 function draw() {
 
+  genKey();
   genDraw();
+
+}
+
+function genKey() {
+
+  noStroke();
+
+  fill('black');
+  text('Change color of background:', 10, 5);
+  text('Red', bgR.x * 2 + bgR.width, 20);
+  text('Green', bgR.x * 2 + bgR.width, 40);
+  text('Blue', bgR.x * 2 + bgR.width, 60);
+
+  fill(bgR.value(), bgG.value(), bgB.value());
+  rect(100 + bgR.width, 20, 40, 40);
+
+  fill('black');
+  text('Change color of drawing tool:', 10, 105);
+  text('Red', lineR.x * 2 + lineR.width, 120);
+  text('Green', lineR.x * 2 + lineR.width, 140);
+  text('Blue', lineR.x * 2 + lineR.width, 160);
+
+  fill(lineR.value(), lineG.value(), lineB.value());
+  rect(100 + lineR.width, 120, 40, 40);
 
 }
 
 function genDraw() {
 
   if (mouseIsPressed){
-    stroke(lineColor);
-    strokeWeight(lineWeight);
+    stroke(lineR.value(), lineG.value(), lineB.value());
+    strokeWeight(lineWeight.value());
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 
@@ -54,5 +92,5 @@ function saveImage() {
 }
 
 function reset() {
-  background(bgR, bgG, bgB);
+  background(bgR.value(), bgG.value(), bgB.value());
 }
