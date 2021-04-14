ab36c4d2863f51e69d88891bec073fab65d54b1c
Winnie Liang <1126winniel@gmail.com>
2021-04-14 01:02:24 -0700

    i think im done :D

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
 
-var canvas;
+let canvas;
 
-var bgR = 80; bgG = 100; bgB = 200;
+let bgRedSlider, bgGreenSlider, bgBlueSlider;
 
-var lineR, lineG, lineB;
-var lineWeight;
+let bgRed = 100, bgGreen = 80, bgBlue = 150;
+
+let backgroundColorDiv;
+let backgroundColorComp;
+
+let lineRedSlider, lineGreenSlider, lineBlueSlider;
+let lineRed = 150, lineGreen = 100, lineBlue = 80;
+
+let lineColorDiv;
+let lineColorComp;
+
+let lineWeightSlider;
 
 let mousePos = [];
+
+let backgroundReset;
 
 let imgSave;
 let imgReset;
@@ -14,85 +26,108 @@ let imgReset;
 function setup() {
 
   frameRate(60);
-  canvas = createCanvas((windowWidth - 600), (windowHeight - 300));
+  canvas = createCanvas((800), (800));
   canvas.parent('sketch-holder');
 
-  bgR = createSlider(0, 255, 180);
-  bgR.position(10, 20);
-  bgG = createSlider(0, 255, 100);
-  bgG.position(10, 40);
-  bgB = createSlider(0, 255, 200);
-  bgB.position(10, 60);
+  bgRedSlider = createSlider(0, 255, 200);
+  bgRedSlider.parent('background-red');
+  bgRedSlider.style('width', '150px');
+  bgRed = bgRedSlider.value();
 
-  // bgR = parseInt(document.getElementById('red').value);
-  // bgG = parseInt(document.getElementById('green').value);
-  // bgB = parseInt(document.getElementById('blue').value);
+  bgGreenSlider = createSlider(0, 255, 150);
+  bgGreenSlider.parent('background-green');
+  bgGreenSlider.style('width', '150px');
+  bgGreen = bgGreenSlider.value();
 
-  // background(bgR, bgG, bgB);
+  bgBlueSlider = createSlider(0, 255, 250);
+  bgBlueSlider.parent('background-blue');
+  bgBlueSlider.style('width', '150px');
+  bgBlue = bgBlueSlider.value();
 
-  background(bgR.value(), bgG.value(), bgB.value());
+  backgroundColorDiv = createDiv();
+  backgroundColorDiv.parent("color-block-background");
+  backgroundColorDiv.style('width', '100%');
+  backgroundColorDiv.style('height', '100%');
 
-  lineR = createSlider(0, 255, 200);
-  lineR.position(10, 120);
-  lineG = createSlider(0, 255, 200);
-  lineG.position(10, 140);
-  lineB = createSlider(0, 255, 180);
-  lineB.position(10, 160);
+  backgroundReset = createButton('Update New Background Color');
+  backgroundReset.parent('background-change-button');
+  backgroundReset.mousePressed(reset);
 
-  lineWeight = createSlider(1, 50, 5);
-  lineWeight.position(10, 220);
+  // backgroundColorComp = color(bgRed, bgGreen, bgBlue);
+  // background(backgroundColorComp);
 
-  imgSave = createButton('Save Image As a png');
+  lineRedSlider = createSlider(0, 255, 255);
+  lineRedSlider.parent('line-red');
+  lineRedSlider.style('width', '150px');
+  lineRed = lineRedSlider.value();
+  //lineRedSlider.position(10, 120);
+  lineGreenSlider = createSlider(0, 255, 220);
+  lineGreenSlider.parent('line-green');
+  lineGreenSlider.style('width', '150px');
+  lineGreen = lineGreenSlider.value();
+  //lineGreenSlider.position(10, 140);
+  lineBlueSlider = createSlider(0, 255, 220);
+  lineBlueSlider.parent('line-blue');
+  lineBlueSlider.style('width', '150px');
+  lineBlue = lineBlueSlider.value();
+  //lineBlueSlider.position(10, 160);
+
+  lineColorDiv = createDiv();
+  lineColorDiv.parent("color-block-line");
+  lineColorDiv.style('width', '100%');
+  lineColorDiv.style('height', '100%');
+
+  // lineColorComp = color(lineRed, lineGreen, lineBlue);
+  // background(lineColorComp);
+
+  lineWeightSlider = createSlider(1, 50, 20);
+  lineWeightSlider.parent('line-weight');
+  lineWeightSlider.style('width', '150px');
+  //lineWeightSlider.position(10, 220);
+
+  imgSave = createButton('Save Your Drawing as a png File!');
   imgSave.parent('save-button');
   imgSave.mousePressed(saveImage);
 
-  imgReset = createButton('Reset Image/Update New Background Color');
+  imgReset = createButton('Reset Image');
   imgReset.parent('reset-button');
   imgReset.mousePressed(reset);
+
+  background(bgRed, bgGreen, bgBlue);
 
 }
 
 function draw() {
 
-  genKey();
   genDraw();
-
-}
-
-function genKey() {
-
-  noStroke();
-
-  fill('black');
-  text('Change color of background:', 10, 5);
-  text('Red', bgR.x * 2 + bgR.width, 20);
-  text('Green', bgR.x * 2 + bgR.width, 40);
-  text('Blue', bgR.x * 2 + bgR.width, 60);
-
-  fill(bgR.value(), bgG.value(), bgB.value());
-  rect(100 + bgR.width, 20, 40, 40);
-
-  fill('black');
-  text('Change color of drawing tool:', 10, 105);
-  text('Red', lineR.x * 2 + lineR.width, 120);
-  text('Green', lineR.x * 2 + lineR.width, 140);
-  text('Blue', lineR.x * 2 + lineR.width, 160);
-
-  fill(lineR.value(), lineG.value(), lineB.value());
-  rect(100 + lineR.width, 120, 40, 40);
+  genColorBlock();
 
 }
 
 function genDraw() {
 
   if (mouseIsPressed){
-    stroke(lineR.value(), lineG.value(), lineB.value());
-    strokeWeight(lineWeight.value());
+    stroke(lineRedSlider.value(), lineGreenSlider.value(), lineBlueSlider.value());
+    strokeWeight(lineWeightSlider.value());
     line(mouseX, mouseY, pmouseX, pmouseY);
     mousePos.push([mouseX, mouseY]);
   }
 
-  console.log(mousePos);
+}
+
+function genColorBlock() {
+
+  bgRed = bgRedSlider.value();
+  bgGreen = bgGreenSlider.value();
+  bgBlue = bgBlueSlider.value();
+  backgroundColorComp = color(bgRed, bgGreen, bgBlue);
+  backgroundColorDiv.style('background-color', backgroundColorComp);
+
+  lineRed = lineRedSlider.value();
+  lineGreen = lineGreenSlider.value();
+  lineBlue = lineBlueSlider.value();
+  lineColorComp = color(lineRed, lineGreen, lineBlue);
+  lineColorDiv.style('background-color', lineColorComp);
 
 }
 
@@ -100,14 +135,10 @@ function saveImage() {
   saveCanvas(canvas, 'myDrawing', 'png');
 }
 
-// function reset() {
-//
-//   bgR = parseInt(document.getElementById('red').value);
-//   bgG = parseInt(document.getElementById('green').value);
-//   bgB = parseInt(document.getElementById('blue').value);
-//
-// }
-
 function reset() {
-  background(bgR.value(), bgG.value(), bgB.value());
+  background(bgRed, bgGreen, bgBlue);
+}
+
+function windowResized() {
+  resizeCanvas((windowWidth * 0.75), (windowHeight * 0.75), true);
 }
