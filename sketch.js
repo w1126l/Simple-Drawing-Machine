a108296b1da852641bfa2e5180ab9f8159c86faf
Winnie Liang <1126winniel@gmail.com>
2021-04-15 02:08:13 -0700

    perlin, alpha edits

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 let canvas;
 
 // background stuff
@@ -26,9 +25,17 @@ let imgReset; // resets sketch
 
 let mousePos = []; // 3.5 array of old mouse positions
 
-//let bgFade; // 3.10 variable for alpha fade
+let bgFade; // 3.10 variable for alpha fade
 
-//let n; // 3.12 variable for perlin noise
+let n; // 3.12 variable for perlin noise
+
+var strokeRed;
+var strokeGreen;
+var strokeBlue;
+
+//TESTING!!!!!!!!!!!!!!!!!!!!!!
+let randBgRed, randBgGreen, randBgBlue;
+let randLineRed, randLineGreen, randLineBlue;
 
 function setup() {
 
@@ -38,16 +45,21 @@ function setup() {
   //canvas = createCanvas(windowWidth, windowHeight); // 3.9 making canvas fullscreen
   canvas.parent('sketch-holder');
 
+  //randInitialColor();
+
+  //bgRedSlider = createSlider(0, 255, randBgRed);
   bgRedSlider = createSlider(0, 255, 200);
   bgRedSlider.parent('background-red');
   bgRedSlider.style('width', '150px');
   bgRed = bgRedSlider.value();
 
-  bgGreenSlider = createSlider(0, 255, 150);
+  //bgGreenSlider = createSlider(0, 255, randBgGreen);
+  bgGreenSlider = createSlider(0, 255, 100);
   bgGreenSlider.parent('background-green');
   bgGreenSlider.style('width', '150px');
   bgGreen = bgGreenSlider.value();
 
+  //bgBlueSlider = createSlider(0, 255, randBgBlue);
   bgBlueSlider = createSlider(0, 255, 250);
   bgBlueSlider.parent('background-blue');
   bgBlueSlider.style('width', '150px');
@@ -62,17 +74,20 @@ function setup() {
   backgroundReset.parent('background-change-button');
   backgroundReset.mousePressed(reset);
 
-  lineRedSlider = createSlider(0, 255, 255);
+  //lineRedSlider = createSlider(0, 255, randLineRed);
+  lineRedSlider = createSlider(0, 255, 250);
   lineRedSlider.parent('line-red');
   lineRedSlider.style('width', '150px');
   lineRed = lineRedSlider.value();
 
-  lineGreenSlider = createSlider(0, 255, 220);
+  //lineGreenSlider = createSlider(0, 255, randLineGreen);
+  lineGreenSlider = createSlider(0, 255, 200);
   lineGreenSlider.parent('line-green');
   lineGreenSlider.style('width', '150px');
   lineGreen = lineGreenSlider.value();
 
-  lineBlueSlider = createSlider(0, 255, 220);
+  //lineBlueSlider = createSlider(0, 255, randLineBlue);
+  lineBlueSlider = createSlider(0, 255, 100);
   lineBlueSlider.parent('line-blue');
   lineBlueSlider.style('width', '150px');
   lineBlue = lineBlueSlider.value();
@@ -85,7 +100,6 @@ function setup() {
   lineWeightSlider = createSlider(1, 50, 20);
   lineWeightSlider.parent('line-weight');
   lineWeightSlider.style('width', '150px');
-  lineWeight = lineWeightSlider.value();
 
   imgSave = createButton('Save Your Drawing as a png File!');
   imgSave.parent('save-button');
@@ -97,7 +111,7 @@ function setup() {
 
   background(bgRed, bgGreen, bgBlue);
 
-  //n = 0; // 3.12 variable for perlin noise
+  n = 0; // 3.12 variable for perlin noise
 }
 
 function draw() {
@@ -109,24 +123,37 @@ function draw() {
 
 function genDraw() {
 
-  if(mouseIsPressed){
+  if (mouseIsPressed) {
 
+    if(mouseButton === LEFT) { // normal draw tool
+      strokeRed = lineRed;
+      strokeGreen = lineGreen;
+      strokeBlue = lineBlue;
+    }
+
+    if (keyIsDown(SHIFT)) { // eraser tool
+      strokeRed = bgRed;
+      strokeGreen = bgGreen;
+      strokeBlue = bgBlue;
+    }
+
+    if (keyIsDown(ALT)) { // 3.10 variable for alpha fade
+      bgFade = color(bgRed, bgGreen, bgBlue, 5);
+      background(bgFade);
+    }
+
+    if (keyIsDown(CONTROL)) { // 3.12 variable for perlin noise
+      strokeRed = (lineRed * noise(n+10));
+      strokeGreen = (lineGreen * noise(n+15));
+      strokeBlue = (lineBlue * noise(n+20));
+    }
+
+    lineWeight = lineWeightSlider.value();
     strokeWeight(lineWeight);
-
-    if(mouseButton === LEFT) {
-      // bgFade = color(bgRed, bgGreen, bgBlue, 5); // 3.10 variable for alpha fade
-      // background(bgFade); // 3.10 for alpha fade
-      //stroke((lineRed * noise(n+10)), (lineGreen * noise(n+15)), (lineBlue * noise(n+20))); // 3.12 variable for perlin noise
-      stroke(lineRed, lineGreen, lineBlue);
-      //n += 0.01;
-    }
-
-    if(keyIsPressed === true) {
-      stroke(bgRed, bgGreen, bgBlue);
-    }
-
+    stroke(strokeRed, strokeGreen, strokeBlue);
     line(mouseX, mouseY, pmouseX, pmouseY);
     mousePos.push([mouseX, mouseY]);
+    n += 0.01;
 
   }
 }
@@ -157,4 +184,22 @@ function reset() {
 
 function windowResized() {
   resizeCanvas((windowWidth * 0.75), (windowHeight * 0.75), true);
+}
+
+function randInitialColor() {
+
+  noLoop();
+
+  randBgRed = int(random(100, 256));
+  randBgGreen = int(random(100, 256));
+  randBgBlue = int(random(100, 256));
+
+  console.log("Initiation color background: (" + randBgRed + ", " + randBgGreen + ", " + randBgBlue + ")");
+
+  randLineRed = int(random(100, 256));
+  randLineGreen = int(random(100, 256));
+  randLineBlue = int(random(100, 256));
+
+  console.log("Initiation color line: (" + randLineRed + ", " + randLineGreen + ", " + randLineBlue + ")");
+
 }
